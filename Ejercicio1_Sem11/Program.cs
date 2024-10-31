6bbab95146f60a5d126e8c8d94b3a7a71e05c91c
Runa <Runa@DESKTOP-ROMFS86>
2024-10-30 22:47:15 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1_Sem11/Program.cs
@@ -0,0 +1,33 @@
+﻿//N00458733
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Sem11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Ingrese el número entero de orden {i+1} en el arreglo: ");
+                while(!int.TryParse(Console.ReadLine(), out numeros[i]))
+                {
+                    Console.WriteLine("Recuerda que el número debe ser entero.");
+                    Console.Write($"Ingrese el número entero de orden {i + 1} en el arreglo: ");
+                }
+                
+            }
+            Console.Write("Los números ingresados son: ");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"{numeros[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
